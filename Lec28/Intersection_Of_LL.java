92473e48fea5eb9cf0ce208bf08a3a35470b4ff2
harshfi <harshcooltripathi1@gmail.com>
2025-08-01 14:37:58 +0530

    LL Qps

---
+++ Lec28/Intersection_Of_LL.java
@@ -0,0 +1,10 @@
+package Lec28;
+
+public class Intersection_Of_LL {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Lec28/Sort_Linked_List.java
@@ -0,0 +1,10 @@
+package Lec28;
+
+public class Sort_Linked_List {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
